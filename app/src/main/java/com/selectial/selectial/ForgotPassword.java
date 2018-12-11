25a8661fb49be866309f680d5e0cc2a81af9fa85
nisha <nisharasiya13@gmail.com>
2018-12-11 17:23:24 +0530

    fsdfsd

--- app/src/main/java/com/selectial/selectial/ForgotPassword.java
+++ app/src/main/java/com/selectial/selectial/ForgotPassword.java
@@ -2,12 +2,22 @@ package com.selectial.selectial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ForgotPassword extends AppCompatActivity {
+
+
+    EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        email = findViewById(R.id.editText);
+
+        String e = email.getText().toString();
+
+
     }
 }
--- app/src/main/java/com/selectial/selectial/Settings.java
+++ app/src/main/java/com/selectial/selectial/Settings.java
@@ -1,13 +1,33 @@
 package com.selectial.selectial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.selectial.selectial.util.SharePreferenceUtils;
 
 public class Settings extends AppCompatActivity {
+
+    TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        logout = findViewById(R.id.textView75);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Settings.this , SignupLogin.class);
+                SharePreferenceUtils.getInstance().deletePref();
+                startActivity(i);
+                finishAffinity();
+
+            }
+        });
     }
 }
